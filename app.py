65e897aa0732c77d2ab6b894820afada22e6fd1b
PervezAhmed <vvit20bq1a05i8@gmail.com>
2024-02-05 09:31:48 +0530

    Added input tags for taking values using html

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy
 import pandas
 
-app  = Flask(__name__)
+app  = Flask(__name__, template_folder='templates', static_folder='static')
 
 model = pickle.load(open('model.pkl','rb'))
 
@@ -14,15 +14,18 @@ def index():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-    input_text = request.form['text']
-    input_text_sp = input_text.split(',')
+    input_text_sp=[]
+    for key, value in request.form.items():
+        input_text_sp.append(value)
+        # print("key: {0}, value: {1}".format(key, value))
+
     np_data = np.asarray(input_text_sp, dtype=np.float32)
     prediction = model.predict(np_data.reshape(1,-1))
 
     if prediction == 1:
         output = "This person has a parkinson disease"
     else:
-        output = "this person has no parkinson disease"
+        output = "This person has no parkinson disease"
 
     return render_template("index.html", message= output)
 
